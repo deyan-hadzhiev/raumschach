043c8ef73858a5b6274cfc0dea02d5f851f720e6
Deyan Hadzhiev <deyan.z.hadzhiev@gmail.com>
2014-01-11 17:25:35 +0200

    Project: Added a string class
    * Implemented class CharString for dynamic string allocation and string operations.

---
+++ project/raumschach/raumschach/include/charstring.h
@@ -0,0 +1,34 @@
+#ifndef __CHARSTRING_H__
+#define __CHARSTRING_H__
+
+class CharString
+{
+public:
+	CharString(const char * src = nullptr);
+	CharString(const CharString& copy);
+	~CharString();
+
+	CharString& operator=(const CharString& assign);
+
+	const CharString& operator+=(const CharString& rhs);
+
+	friend const CharString operator+(const CharString& lhs, const CharString& rhs);
+
+	// element accessor (const)
+	const char& operator[](int index) const;
+	// element accessor
+	char& operator[](int index);
+
+	int Length() const;
+	
+	operator const char* () const;
+	operator char* ();
+	const char* GetPtr() const;
+	char* GetPtr();
+
+private:
+	char * data;
+	int size;
+};
+
+#endif // __CHARSTRING_H__
